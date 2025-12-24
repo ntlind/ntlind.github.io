5532b1dd88a209cc663838cdfa4dd0f1147ac0e9
Nick <nthorlind@gmail.com>
2025-12-24 13:34:18 -0700

    Updates

---
+++ assets/Email my distributor if any of these shelves become empty-C1spbkmc.js
@@ -0,0 +1,1 @@
+const e="/assets/Email%20my%20distributor%20if%20any%20of%20these%20shelves%20become%20empty-xPuzjkIZ.jpg";export{e as default};
---
+++ assets/Text the lot manager if a person stays in frame for more than 4 minutes-BZFr0St3.js
@@ -0,0 +1,1 @@
+const e="/assets/Text%20the%20lot%20manager%20if%20a%20person%20stays%20in%20frame%20for%20more%20than%204%20minutes-B6OCzBcs.webm";export{e as default};
